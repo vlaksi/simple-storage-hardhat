b62d5e330c7a057ba607e6db9f729236e9d6a2fb
Vladislav Maksimovic <daksrapsody@gmail.com>
2022-09-01 23:05:57 +0200

    feature: verify contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,7 @@
 // imports
 const { ethers, run, network } = require('hardhat');
+const GOERLI_CHAIN_ID = 5;
+const AMOUNT_OF_BLOCK_TO_WAIT = 6;
 
 // async main
 async function main() {
@@ -8,22 +10,41 @@ async function main() {
   const simpleStorage = await SimpleStorageFactory.deploy();
   await simpleStorage.deployed();
   console.log(`Deployed contract to: ${simpleStorage.address}`);
-  // // what happens when we deploy to our hardhat network?
-  // if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
-  //   console.log('Waiting for block confirmations...');
-  //   await simpleStorage.deployTransaction.wait(6);
-  //   await verify(simpleStorage.address, []);
-  // }
+  if (
+    network.config.chainId === GOERLI_CHAIN_ID &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    console.log('Waiting for block confirmations...');
+    await simpleStorage.deployTransaction.wait(AMOUNT_OF_BLOCK_TO_WAIT); // we wait because we want to be sure that everything is deployed properly & we can start verifyingß
+    await verify(simpleStorage.address, []);
+  }
 
-  // const currentValue = await simpleStorage.retrieve();
-  // console.log(`Current Value is: ${currentValue}`);
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`Current Value is: ${currentValue}`);
 
-  // // Update the current value
-  // const transactionResponse = await simpleStorage.store(7);
-  // await transactionResponse.wait(1);
-  // const updatedValue = await simpleStorage.retrieve();
-  // console.log(`Updated Value is: ${updatedValue}`);
+  // Update the current value
+  const transactionResponse = await simpleStorage.store(7);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`Updated Value is:  ${updatedValue}`);
 }
+
+// async function verify(contractAddress, args) {
+const verify = async (contractAddress, args) => {
+  console.log('Verifying contract...');
+  try {
+    await run('verify:verify', {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes('already verified')) {
+      console.log('Already Verified!');
+    } else {
+      console.log(e);
+    }
+  }
+};
 
 // main
 main()
