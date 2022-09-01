c770465a201116c06bddd6b160dab46f3e21353a
Vladislav Maksimovic <daksrapsody@gmail.com>
2022-09-01 12:38:02 +0200

    feature: deployable to goerli network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const GOERLI_CHAIN_ID = 5;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  defaultNetwork: 'hardhat',
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: GOERLI_CHAIN_ID,
+    },
+  },
+  solidity: '0.8.9',
 };
