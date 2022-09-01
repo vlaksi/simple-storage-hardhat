e1bc6a5cdf54f012faccb91031127781ad0ace10
Vladislav Maksimovic <daksrapsody@gmail.com>
2022-09-01 23:35:31 +0200

    feature: deploy to the localhost

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require('@nomicfoundation/hardhat-toolbox');
 require('dotenv').config();
 require('./tasks/block-number');
+require('@nomiclabs/hardhat-etherscan');
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
@@ -15,6 +16,10 @@ module.exports = {
       url: GOERLI_RPC_URL,
       accounts: [PRIVATE_KEY],
       chainId: GOERLI_CHAIN_ID,
+    },
+    localhost: {
+      url: 'http://127.0.0.1:8545/',
+      chainId: 31337,
     },
   },
   solidity: '0.8.9',
